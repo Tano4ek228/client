48ea385eaf5ca6d6e85d2c6b232554dac1c040ff
Баяндин Иван <bajandin2015@mail.ru>
2022-06-22 18:58:24 +0300

    3

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname,'build')))
 
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
 
 app.listen(PORT)
