f011e0e3382fb6199ba2974afb7d83bad7ee7d12
Баяндин Иван <bajandin2015@mail.ru>
2022-06-23 01:04:42 +0300

    3

---
+++ src/components/Tovar.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Context } from "..";
+
+const Tovar = () => {
+    const { device } = useContext(Context)
+	return (
+		<div>
+			<div className="tovar_name_line">
+				<h2>Тип</h2>
+				<h2>Цена</h2>
+				<h2>НДС</h2>
+			</div>
+            <table className="table_tovar">
+            {device.TypeDevice.map(type => 		
+                <tr className="tovar_tr">
+                    <tb className="tovar_tb tb_right_botder">
+                    {type.name}
+                    </tb>
+                    <tb className="tovar_tb tb_right_botder">
+                    {type.price}
+                    </tb>
+                    <tb className="tovar_tb">
+                    {type.NDS}
+                    </tb>
+                </tr>
+                )}
+            </table>
+		</div>
+	)
+}
+
+export default Tovar;
